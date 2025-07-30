a167af8356a7be81847682ccab268197a0d7e85e
AmaseCocoa <cocoa@amase.cc>
2025-07-30 13:32:13 +0900

    minifyが失敗した場合に無視する

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import os
+import subprocess
 
 import minify_html
 
+def get_git_commit_hash() -> str:
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    out = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=cwd)
+    return out.strip().decode('ascii')
+
 os.makedirs("./dist", exist_ok=True)
 with open("./index.html", "r", encoding="utf-8") as f:
-    minified = minify_html.minify(f.read(), minify_js=True, minify_css=True, remove_processing_instructions=True, minify_doctype=True)
+    content = f.read().replace("[build]", get_git_commit_hash())
+    try:
+        minified = minify_html.minify(content.replace("[isMinified]", "true"), minify_js=True, minify_css=True, remove_processing_instructions=True)
+    except:  # noqa: E722
+        minified = content.replace("[isMinified]", "false")
     with open("./dist/index.html", "w", encoding="utf-8") as f:
         f.write(minified)
